12e459814609a580e86d63e5d074ae4cd76ff2ee
Maham Javed <mahamjaved994@gmail.com>
2025-10-14 19:24:15 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route,Navigate  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+
 import WhatsAppButton from "./components/WhatsAppButton";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -15,17 +16,21 @@ function App() {
   return (
     <Router basename="/visa-travel-frontend">
       <Navbar /> {/* Navbar inside Router */}
+      
       <Routes>
+        {/* Redirect root URL to /home */}
         <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/" element={<Home />} /> {/* Home page */}
+        <Route path="/home" element={<Home />} /> {/* Home page */}
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-          {/* Optional: catch all unknown routes */}
+        
+        {/* Catch-all route for invalid URLs */}
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
+
       <Footer /> {/* Footer inside Router */}
-       <WhatsAppButton />  {/* Floating WhatsApp Chat */}
+      <WhatsAppButton /> {/* Floating WhatsApp Chat */}
     </Router>
   );
 }
